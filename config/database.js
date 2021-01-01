1e56a55f65a37ac2f6dda57f82e23cccd6f21539
earlwlkr <mike358015@gmail.com>
2021-01-01 23:26:29 +0700

    Bootstrap with PG

--- config/database.js
+++ config/database.js
@@ -1,11 +1,38 @@
+const url = require('url');
+
+function getConfig(env) {
+  const settings = {
+    client: 'postgres',
+    host: env('DATABASE_HOST'),
+    port: env.int('DATABASE_PORT'),
+    database: env('DATABASE_NAME'),
+    username: env('DATABASE_USERNAME'),
+    password: env('DATABASE_PASSWORD'),
+  };
+
+  if (process.env.DATABASE_URL) {
+    const parsed = url.parse(process.env.DATABASE_URL, true);
+    const [username, password] = parsed.auth.split(':');
+
+    settings.host = parsed.hostname;
+    settings.port = Number(parsed.port);
+    settings.database = parsed.pathname.substr(1);
+    settings.username = username;
+    settings.password = password;
+    settings.ssl = false;
+  }
+  return settings;
+}
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        schema: 'public',
+        ...getConfig(env),
       },
       options: {
         useNullAsDefault: true,
---
+++ config/env/production/database.js
@@ -0,0 +1,29 @@
+const url = require('url');
+
+const settings = {
+  client: 'postgres',
+  timezone: 'Asia/Ho_Chi_Minh',
+};
+
+if (process.env.DATABASE_URL) {
+  const parsed = url.parse(process.env.DATABASE_URL, true);
+  const [username, password] = parsed.auth.split(':');
+
+  settings.host = parsed.hostname;
+  settings.port = Number(parsed.port);
+  settings.database = parsed.pathname.substr(1);
+  settings.username = username;
+  settings.password = password;
+  settings.ssl = false;
+}
+
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings,
+      options: {},
+    },
+  },
+});
