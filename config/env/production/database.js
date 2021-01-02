923b3cf65eac56b96442600d960fc61bf6741110
Huynh.Huynh <huynh.huynh@nab.com.au>
2021-01-02 09:53:33 +0700

    Off SSL

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -14,7 +14,7 @@ if (process.env.DATABASE_URL) {
   settings.database = parsed.pathname.substr(1);
   settings.username = username;
   settings.password = password;
-  settings.ssl = true;
+  settings.ssl = false;
 }
 
 module.exports = ({ env }) => ({
